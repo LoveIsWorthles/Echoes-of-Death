4f8f32bcc085cee0ee638aad15fd0838b477197d
Dragosharwild <79346195+Dragosharwild@users.noreply.github.com>
2026-05-06 15:13:49 -0600

    weapons setup
    Created folders and placeholder files for planned weapons system. Also did some light reorganizing of other files.

---
+++ Assets/Scripts/Combat/Weapons/Behaviors/ProjectileWeaponBehavior.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ProjectileWeaponBehavior : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
